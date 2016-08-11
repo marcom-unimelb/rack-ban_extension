c50df0880e05e028dd247e31035d822cc1f30bb0
neilang <neilang@gmail.com>
2016-08-11 13:34:52 +1000

    Added some simple tests

---
+++ test/test_ban_extension.rb
@@ -0,0 +1,57 @@
+require 'rubygems'
+require 'bundler/setup'
+
+require 'minitest/autorun'
+require 'rack/test'
+require 'rack/ban_extension'
+
+class TestBanExtension < Minitest::Test
+  include Rack::Test::Methods
+
+  def app
+    Rack::Builder.new {
+      use Rack::BanExtension, %w(php aspx)
+      run ->(_) { [200, {}, ['Hello World!']] }
+    }.to_app
+  end
+
+  def test_valid_request_without_extension
+    get '/'
+    assert last_response.ok?
+  end
+
+  def test_valid_request
+    get '/index.html'
+    assert last_response.ok?
+  end
+
+  def test_valid_params_featuring_extension
+    get '/index.html?.php'
+    assert last_response.ok?
+  end
+
+  def test_valid_anchor_featuring_extension
+    get '/index.html#.php'
+    assert last_response.ok?
+  end
+
+  def test_banned_extension
+    get '/index.php'
+    assert last_response.bad_request?
+  end
+
+  def test_additional_banned_extension
+    get '/index.aspx'
+    assert last_response.bad_request?
+  end
+
+  def test_banned_extension_with_params
+    get '/index.php?.html'
+    assert last_response.bad_request?
+  end
+
+  def test_banned_extension_with_anchor
+    get '/index.php#.html'
+    assert last_response.bad_request?
+  end
+end
